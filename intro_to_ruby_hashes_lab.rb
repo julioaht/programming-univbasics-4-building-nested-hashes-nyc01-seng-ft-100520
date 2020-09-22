cb0afff6b5be4bba5d3713de8037b78ece101734
julio hernandez <julioandres72@gmail.com>
2020-09-22 14:10:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
-   { :railroads =>
+  monopoly = {
+    :railroads =>
   }
-  base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
